03dc15d9cbaf33e325a6ca81b605b3ba2fdc6728
koechronix <koechronix@yahoo.com>
2022-07-16 22:23:00 -0400

    done

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,43 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
+
 
 function ShoppingList({ items }) {
-  return (
-    <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleFilterChange(event) {
+    // event.target.value will be the value selected by the user
+    setSelectedCategory(event.target.value);
+    return selectedCategory;
+  }
+
+  // we want to filter the items to only display the ones based on the selected category
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") {
+    return true;
+    }else {
+     return item.category === selectedCategory;
+    }
+  });
+
+  // const groceryItemList = itemsToDisplay.map((item) =>(
+  //   <Item name={item.name} category={item.category} key={item.id}></Item>
+  // ));
+
+  return(
+    <div className="ShoppingLIst">
+      <div className="Filter">      
+          <select name="filter" onChange={handleFilterChange}>
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+          </select>
+       
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
